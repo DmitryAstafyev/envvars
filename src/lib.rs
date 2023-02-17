fee9d94397dcff2eecfd7c2855b25b3144c341fb
DmitryAstafyev <dvastafyev@gmail.com>
2023-02-17 15:27:09 +0100

    Documentation

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,18 @@ lazy_static! {
     pub static ref EXTRACTOR: Extractor = Extractor::new();
 }
 
+/// Extract environment variables without shell context.
+///
+/// # Examples
+///
+/// ```
+/// use std::collections::HashMap;
+/// use envvars::get_context_envvars;
+///
+/// let vars: HashMap<String, String> = get_context_envvars().unwrap();
+///
+/// assert!(vars.contains_key("PATH"));
+/// ```
 pub fn get_context_envvars() -> Result<HashMap<String, String>, Error> {
     EXTRACTOR.get(None, &Vec::new())
 }
--- src/profiles/mod.rs
+++ src/profiles/mod.rs
@@ -7,15 +7,32 @@ use std::{
 pub mod unix;
 pub mod windows;
 
+/// Definition of shell profile
 #[derive(Debug, Clone)]
 pub struct Profile {
+    /// Suggested name of shell. For unix based systems it will be name of executable file,
+    /// like "bash", "fish" etc. For windows it will be names like "GitBash", "PowerShell"
+    /// etc.
     pub name: String,
+    /// Path to executable file of shell
     pub path: PathBuf,
+    /// List of environment variables. Because extracting operation could take some time
+    /// by default `envvars = None`. To load data should be used method `load`, which will
+    /// make attempt to detect environment variables.
     pub envvars: Option<HashMap<String, String>>,
+    /// Private field to store arguments needed to execute shell in right way to grab list
+    /// of environment variables
     args: Vec<String>,
 }
 
 impl Profile {
+    /// Creates shell's profile description
+    /// * `shell` - path to shell's executable file
+    /// * `args` - list of arguments needed to pass a command into shell. For example: "-c"
+    /// to have a full command like: "/etc/bin/bash -c cmd"
+    /// * `name` - optional name for profile. For unix based systems it will be name of
+    /// executable file, like "bash", "fish" etc. For windows better to provide name to
+    /// have it like "GitBash", "PowerShell" etc.
     pub fn new(shell: &PathBuf, args: Vec<&str>, name: Option<&str>) -> Result<Self, Error> {
         let path = Path::new(shell);
         if !path.exists() {
@@ -42,12 +59,59 @@ impl Profile {
         })
     }
 
+    /// Makes attempt to grab a list of environment variables for profile. It will
+    /// spawn an instance of shell with extractor as command argument. If stdout will
+    /// have suitable output, it will be parsed and list of environment variables will
+    /// be saved in `self.envvars`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::{path::PathBuf, str::FromStr};
+    /// use envvars::Profile;
+    ///
+    /// let mut profile: Profile = Profile::new(&PathBuf::from_str("/bin/bash").unwrap(), vec!["-c"], None).unwrap();
+    ///
+    /// assert_eq!(profile.name, "bash");
+    ///
+    /// profile.load().unwrap();
+    ///
+    /// assert!(profile.envvars.is_some());
+    ///
+    /// if let Some(vars) = profile.envvars.as_ref() {
+    ///     assert!(vars.contains_key("PATH"));
+    /// }
+    /// ```
     pub fn load(&mut self) -> Result<(), Error> {
         self.envvars = Some(EXTRACTOR.get(Some(&self.path), &self.args)?);
         Ok(())
     }
 }
 
+/// Returns all detected shell's profiles.
+/// Unix based systems:
+/// - reads /etc/shells and creates Profile for each found shell
+/// Windows:
+/// - checks most regulars shells like CMD, PowerShell, GitBash, Cygwin etc.
+///
+/// Because an operation of extracting of environment variables could take some time,
+/// by default `Profile.envvars` is empty (None). To load data should be used method
+/// `Profile.load`, which will make attempt to detect environment variables.
+/// 
+/// # Examples
+///
+/// ```
+/// use envvars::{get_profiles, Profile};
+///
+/// let mut profiles: Vec<Profile> = get_profiles().unwrap();
+///
+/// profiles.iter_mut().for_each(|profile| {
+///     // Attempt to load envvars
+///     if let Err(err) = profile.load() {
+///         eprintln!("Cannot load envvars for {}: {err}", profile.name);
+///     }
+/// });
+/// ```
 pub fn get() -> Result<Vec<Profile>, Error> {
     if cfg!(windows) {
         windows::get()
